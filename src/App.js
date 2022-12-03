eac025d3364b5375b3c8a87ced50076cce8c06f7
devhowey <dev.howey@gmail.com>
2022-12-02 19:59:22 -0600

    I was adding, commiting, and pushing from the components directory...

--- src/App.js
+++ src/App.js
@@ -1,24 +1,54 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
+import { Routes, Route } from 'react-router-dom'
 import './App.css';
+import AddTodoForm from './components/AddTodoForm';
+import Navbar from './components/Navbar';
+import NotFound from './components/NotFound';
+import TodoList from './components/TodoList';
+
+const useTodoListState = () => {
+  const [todoList, setTodoList] = useState(() => {
+    const savedTodoList = JSON.parse(localStorage.getItem('savedTodoList'))
+    return savedTodoList || []
+  })
+
+  useEffect(() => {
+    localStorage.setItem('savedTodoList', JSON.stringify(todoList))
+  }, [todoList])
+
+  return [todoList, setTodoList]
+}
 
 function App() {
+  const [todoList, setTodoList] = useTodoListState()
+
+  const addTodo = (newTodo) => {
+    setTodoList([...todoList, newTodo])
+  }
+
+  // TODO: Write this function out to edit a todo list item
+  const editTodo = (todo) => {
+    // some logic needs to go here
+  }
+
+  // TODO: Write this function out to remove a todo list item
+  const removeTodo = (id) => {
+    const newTodoList = todoList.filter(todo => todo.id !== id)
+    setTodoList(newTodoList)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navbar />
+      <div className='container'>
+        <h1>Todo List</h1>
+        <Routes>
+          <Route exact path='/' element={<TodoList todoList={todoList} onRemoveTodo={removeTodo} />} />
+          <Route caseSensitive path='/newTodo' element={<AddTodoForm onAddTodo={addTodo} />} />
+          <Route path='*' element={<NotFound />} />
+        </Routes>
+      </div>
+    </>
   );
 }
 
